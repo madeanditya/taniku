08a77ca328966794f5a01d1bbafc5d1c95b3d74c
WayanPande <yande554@gmail.com>
2021-12-04 20:50:03 +0800

    add style to profile and address page view

--- resources/views/profile/address.blade.php
+++ resources/views/profile/address.blade.php
@@ -7,33 +7,89 @@
 
 {{-- content --}}
 @section('content')
-    <div>
-        <div><a href="/profile">Biodata diri</a></div>
-        <div><a href="/profile/address">Alamat</a></div>
-    </div>
-    <hr>
-    <div class="address">
-        <div>
-            <h3>addresses</h3>
-            <div><a href="/profile/address/create">Tambah alamat</a></div>
+    <div class="profile-wrapper">
+        <ul class="nav nav-tabs nav-fill">
+            <li class="nav-item">
+                <a class="nav-link profile-nav_notActive" aria-current="page" href="/profile">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active profile-nav_active" href="/profile/address">List Alamat</a>
+            </li>
+        </ul>
+        <div class="address">
+            <div class="address-header">
+                <button data-bs-toggle="modal" data-bs-target="#add-new-address" class="btn btn-success">Tambah
+                    alamat</button>
+            </div>
+            <div class="address-card__wrapper">
+                @foreach ($addresses as $address)
+                    <div class="address-card {{ $address->default ? 'address-card__main' : '' }}">
+                        @if ($address->default)
+                            <span>Utama</span>
+                        @endif
+                        <p class="fw-bold">{{ $address->fullname }}</p>
+                        <p>{{ $address->phone_number }}</p>
+                        <p>{{ $address->address . ', ' . $address->subdistrict . ', ' . $address->city . ', ' . $address->province }}
+                        </p>
+                        <p>{{ $address->postal_code }}</p>
+                    </div>
+                @endforeach
+            </div>
         </div>
-        <hr>
-        <div>
-            @foreach ($addresses as $address)
-                <div>{{ $address->fullname }}</div>
-                <div>{{ $address->phone_number }}</div>
-                <div>{{ $address->address . ", " . $address->subdistrict . ", " . $address->city . ", " . $address->province }}</div>
-                <div>{{ $address->postal_code }}</div>
-                <div>{{ $address->default }}</div>
-                <br>
-            @endforeach
-        </div>
     </div>
-    <hr>
 @endsection
+
+<div class="modal fade" id="add-new-address" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable">
+        <form class="modal-content add-address__modal___content" action="/profile/address/create" method="post">
+            <div class="modal-header border-0">
+                <h5 class="modal-title fw-bold">Tambah alamat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                @csrf
+                <div class="mb-3">
+                    <label for="fullname" class="form-label fw-bold">Fullname</label>
+                    <input type="text" class="form-control" id="fullname" name="fullname" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="phone_number" class="form-label fw-bold">Phone Number</label>
+                    <input type="number" class="form-control" id="phone_number" name="phone_number"
+                        value="{{ $address->phone_number }}" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="province" class="form-label fw-bold">Province</label>
+                    <input type="text" class="form-control" id="province" name="province" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="city" class="form-label fw-bold">City</label>
+                    <input type="text" class="form-control" id="city" name="city" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="subdistrict" class="form-label fw-bold">Subdistrict</label>
+                    <input type="text" class="form-control" id="subdistrict" name="subdistrict" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label fw-bold">Address</label>
+                    <input type="text" class="form-control" id="address" name="address" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="postal_code" class="form-label fw-bold">Postal Code</label>
+                    <input type="number" class="form-control" id="postal_code" name="postal_code" placeholder="">
+                </div>
+                <input type="hidden" id="username" name="username" value="{{ auth()->user()->username }}">
+            </div>
+            <div class="modal-footer border-0 justify-content-center">
+                <button type="submit" name="submit" class="btn btn-success fw-bold"
+                    style="border-radius: 10px; width: 30%">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+
 
 {{-- footer --}}
 @section('footer')
-    <h3>Footer</h3>
     @include('partitions/footer')
 @endsection
--- resources/views/profile/main.blade.php
+++ resources/views/profile/main.blade.php
@@ -7,30 +7,44 @@
 
 {{-- content --}}
 @section('content')
-    <div>
-        <div><a href="/profile">Biodata diri</a></div>
-        <div><a href="/profile/address">Alamat</a></div>
-        <hr>
+    <div class="profile-wrapper">
+        <div class="biodata">
+            <ul class="nav nav-tabs nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link active profile-nav_active" aria-current="page" href="/profile">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link profile-nav_notActive" href="/profile/address">List Alamat</a>
+                </li>
+            </ul>
+            <div class="biodata-wrapper">
+                <div class="biodata-img">
+                    <img src="https://avatars.dicebear.com/api/gridy/{{ $user->username }}.svg"
+                        alt="{{ $user->username }}">
+                </div>
+                <div class="biodata-detail">
+                    <h4>Biodata diri</h4>
+                    <div class="biodata-detail__content">
+                        <div class="biodata-detail__content___value">
+                            <p>Username</p>
+                            <p>Nama</p>
+                            <p>Email</p>
+                            <p>No Hp</p>
+                        </div>
+                        <div class="biodata-detail__content___value">
+                            <p>{{ $user->username }}</p>
+                            <p>{{ $user->fullname }}</p>
+                            <p>{{ $user->email }}</p>
+                            <p>{{ $user->phone_number }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="biodata">
-        <h3>Biodata</h3>
-        <div>{{ $user->username }}</div>
-        <div>{{ $user->fullname }}</div>
-        <div>{{ $user->email }}</div>
-        <div>{{ $user->phone_number }}</div>
-        {{--
-            nama
-            tanggal lahir
-            jenis kelamin
-            email
-            nomor hp
-        --}}
-    </div>
-    <hr>
 @endsection
 
 {{-- footer --}}
 @section('footer')
-    <h3>Footer</h3>
     @include('partitions/footer')
 @endsection
