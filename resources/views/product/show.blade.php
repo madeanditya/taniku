068dae729e05a10f1ef1a0fe4d8634f289e94ebb
agusari31 <agusarinegara@gmail.com>
2021-12-04 11:46:26 +0800

    tabel produk

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -7,18 +7,36 @@
 
 {{-- content --}}
 @section('content')
-    <h3>Content</h3>
-    <div><a href="/product/create">Insert</a></div>
+<div><a href="/product/create" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>Insert</a></div>
+    
     <hr>
-    @foreach ($products as $product)
-        <div>{{ $product->name }}</div>
-        <div>{{ $product->supplier }}</div>
-        <div>{{ $product->price }}</div>
-        <div>{{ $product->description }}</div>
-        <div><a href="/product/edit/{{ $product->id }}">Edit</a></div>
-        <div><a href="/product/destroy/{{ $product->id }}">Delete</a></div>
-        <hr>
-    @endforeach
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Nama Produk</th>
+                <th scope="col">Supplier</th>
+                <th scope="col">Harga</th>
+                <th scope="col">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            @foreach ($products as $product)
+                <th scope="row">{{ $product->id }}</th>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->supplier }}</td>
+                <td>{{ $product->price }}</td>
+                <td>
+                    <a class="btn btn-primary" href="/product/edit/{{ $product->id }}">Edit</a>
+                    <a class="btn btn-danger" href="/product/destroy/{{ $product->id }}">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
 {{-- footer --}}
