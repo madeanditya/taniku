537fcb209e5afabbd4aa5161d490d6dc8d725685
agusari31 <agusarinegara@gmail.com>
2021-12-08 23:33:48 +0800

    update insert product view

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -7,69 +7,73 @@
 
 {{-- content --}}
 @section('content')
-    <div class="content">
-        <form action="/product/store" method="post" id="product-insertion-form">
-            @csrf
-            <h3>Input data produk</h3>
-            <table>
-                <tr>
-                    <td><label for="name">Nama</label></td>
-                    <td><input type="text" name="name" id="name" required></td>
-                    @error('name')
-                    <td class="error-message">{{ $message }}</td>
-                    @enderror
-                </tr>
-                <tr>
-                    <td><label for="description">Deskripsi</label></td>
-                    <td><textarea name="description" id="description" cols="25" rows="4" form="product-insertion-form" required></textarea></td>
-                    @error('description')
-                    <td class="error-message">{{ $message }}</td>
-                    @enderror
-                </tr>
-                <tr>
-                    <td><label for="price">Harga</label></td>
-                    <td><input type="text" name="price" id="price" required></td>
-                    @error('price')
-                    <td class="error-message">{{ $message }}</td>
-                    @enderror
-                </tr>
-                <tr>
-                    <td><label for="stock">Stok</label></td>
-                    <td><input type="text" name="stock" id="stock" required></td>
-                    @error('stock')
-                    <td class="error-message">{{ $message }}</td>
-                    @enderror
-                </tr>
-                <tr>
-                    <td><label for="category">Kategori</label></td>
-                    <td>
-                        <input list="categories" name="category" id="category" required>
-                        <datalist id="categories">
-                            <option value="daging">
-                            <option value="buah-buahan">
-                            <option value="sayur-sayuran">
-                            <option value="biji-bijian">
-                            <option value="umbi-umbian">
-                            <option value="rempah-rempah">
-                        </datalist>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Weight</td>
-                    <td><input type="text" name="weight" id="weight" required></td>
-                    @error('weight')
-                    <td class="error-message">{{ $message }}</td>
-                    @enderror
-                </tr>
-                <input type="hidden" name="supplier" value="{{ auth()->user()->username }}">
-            </table>
-            <button type="submit" class="submit-btn">Submit</button>
-        </form>
+    <div class="content mt-4">
+        <div class="row">
+            <div class="col-4">
+                <img src="/img/taniku.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-6">
+                <form action="/product/store" method="post" id="product-insertion-form">
+                    @csrf
+                    <h3>Input data produk</h3>
+                    <div class="mb-2">
+                        <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" name="name" id="name" required>
+                        @error('name')
+                            <div class="error-message">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-2">
+                        <label for="description" class="form-label">Deskripsi</label>
+                        <textarea class="form-control" name="description" id="description" cols="25" rows="4" form="product-insertion-form" required></textarea>
+                        @error('description')
+                            <div class="error-message">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-2">
+                        <label for="price" class="form-label">Harga</label>
+                        <input type="text" class="form-control" name="price" id="price" required>
+                        @error('price')
+                            <div class="error-message">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-2">
+                        <label for="stock" class="form-label">Stok</label>
+                        <input type="text" class="form-control" name="stock" id="stock" required>
+                        @error('stock')
+                            <div class="error-message">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-2">
+                        <label for="category" class="form-label">Kategori</label>
+                        <input list="categories" class="form-control" name="category" id="category" required>
+                            <datalist id="categories">
+                                <option value="daging">
+                                <option value="buah-buahan">
+                                <option value="sayur-sayuran">
+                                <option value="biji-bijian">
+                                <option value="umbi-umbian">
+                                <option value="rempah-rempah">
+                            </datalist>
+                    </div>
+                    <div class="mb-2">
+                        <label for="weight" class="form-label">Weight</label>
+                        <input type="text" class="form-control" name="weight" id="weight" required>
+                        @error('weight')
+                            <div class="error-message">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <input type="hidden" name="supplier" value="{{ auth()->user()->username }}">
+                    <div class="d-grid mb-5">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 @endsection
 
 {{-- footer --}}
 @section('footer')
-    <h3>Footer</h3>
     @include('partitions/footer')
 @endsection
