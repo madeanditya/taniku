486db7428de8a50ee94210f678d5ddd8bc7da638
Hendradinata <Hendradinata>
2021-12-15 17:48:16 +0800

    detail produk

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -7,14 +7,53 @@
 
 {{-- content --}}
 @section('content')
+    <br><br>
     <div class="product">
-        <div>name: {{ $product->name }}</div>
-        <div>descrption: {{ $product->description }}</div>        
-        <div>price: {{ $product->price }}</div>
-        <div>stock: {{ $product->stock }}</div>
-        <div>category: {{ $product->category }}</div>
-        <div>weight: {{ $product->weight }}</div>
-        <div>supplier: {{ $product->supplier }}</div>
+        <div align=”middle” class="biodata-img"><br>
+            <img src="{{ asset('img/tumbnail.png') }}" alt="taniku">
+        </div>
+        <div class="biodata-wrapper">
+            <div class="biodata-detail">
+                <div class="biodata-detail__content">
+                    <div class="biodata-detail__content___value">
+                        <table border="0" cellpadding=10 cellspacing=15 align="center">
+                            <tr>
+                                <td><h4>Deatail</h4></td>
+                                <td><h4>Product</h4></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Nama Product:</p></td>
+                                <td><p>{{ $product->name }}</p></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Deskripsi:</p></td>
+                                <td><p align="justify">{{ $product->description }}</p></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Harga:</p></td>
+                                <td><p>{{ $product->price }}</p></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Stock:</p></td>
+                                <td><p>{{ $product->stock}}</p></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Kategori:</p></td>
+                                <td><p>{{ $product->category}}</p></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Berat:</p></td>
+                                <td><p>{{ $product->weight}}</p></td>
+                            </tr>
+                            <tr>
+                                <td colspan="7"><p>Supplier:</p></td>
+                                <td><p>{{ $product->supplier }}</p></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
--- resources/views/partitions/navbar.blade.php
+++ resources/views/partitions/navbar.blade.php
@@ -22,27 +22,41 @@
                         <li><a class="dropdown-item" href="/customer_order/show/pending">Customer Orders</a></li>
                     </ul>
                 </li>
+            </ul>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ ($title == "Wishlist | Show") ? 'active' : '' }}"  href="/wishlist/show">
+                        <i class="far fa-list-alt"></i> Wishlist
+                    </a>
+                </li>
+                <li class="nav-item">
+                   <a class="nav-link {{ ($title == "Cart | Show") ? 'active' : '' }}"  href="/cart/show">
+                    <i class="fas fa-shopping-cart"></i> Shooping Cart
+                    </a>
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Profile
+                        <i class="fas fa-user-circle"></i> Profile
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="/user/settings">Settings</a></li>
                         <li><a class="dropdown-item" href="/wishlist/show">Wishlist</a></li>
                         <li><a class="dropdown-item" href="/cart/show">Shopping Cart</a></li>
-                        <li><a class="dropdown-item" href="/order/show/all">Order</a></li>
+                        <li><a class="dropdown-item" href="/order/show">Order</a></li>
                     </ul>
                 </li>
             </ul>
+            <form action="/user/logout" method="post">
+                <div class="text-center text-lg-start">
+                    @csrf
+                    <button type="submit" class="btn btn-success">
+                        Logout
+                    </button>
+                </div>
+            </form>
         </div>
-        <form action="/user/logout" method="post">
-            <div class="text-center text-lg-start">
-                @csrf
-                <button type="submit" class="btn btn-success">
-                    Logout
-                </button>
-            </div>
-        </form>
     </nav>
 @else
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
@@ -67,4 +81,4 @@
         </div>
         </div>
     </nav>
-@endauth
+    @endauth
